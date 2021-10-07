a222863b683b7ace195365cbe486ae6862a6313f
izumi0x01 <izumisohpublic@gmail.com>
2021-10-07 15:56:50 +0900

    connectionstoresコントローラの作成

--- app/models/store.rb
+++ app/models/store.rb
@@ -8,10 +8,5 @@ class Store < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  def is_token_match?(token, client)
-    
-    self.valid_token?(token, client)
-    
-  end
-  
+
 end

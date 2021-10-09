4ef9c24af8062dcc830e652bb5b346aac7252b43
izumi0x01 <izumisohpublic@gmail.com>
2021-10-09 13:07:53 +0900

    メソッドの編集

--- app/controllers/api/v1/stampcard_contents_controller.rb
+++ app/controllers/api/v1/stampcard_contents_controller.rb
@@ -2,45 +2,27 @@ class Api::V1::StampcardsController < ApplicationController
 
     def create
 
-        exist_stampcard = Stampcard.find_by(user_id: stampcards_params.user_id)
-        if exist_stampcard.present?
-            render json: {error: 'stampcard record was already registored'}, status: 404 and return
-        end
-
-        new_stampcard = Stampcard.new(stampcards_params.merge(stamp_count: 0))
-        if new_stampcard.save
-            render json: new_stampcard, status: 200 and return
-        else    
-            render json: {error: 'cant regist stampcard'}, status: 404 and return
-        end
-    end 
+    end
 
     def update
-        exist_stampcard = Stampcard.find_by(user_id: stampcards_params.user_id)
-    end 
 
-    def stamp
+    end
 
-        exist_stampcard = Stampcard.find_by(user_id: stampcards_params.user_idgit)
-        if exist_stampcard.present?
-            render json: {error: 'stampcard record was already registored'}, status: 404 and return
-        end
+    def destroy
 
-        currente_stampcount = exist_stampcard.stamp_count
-        currente_stampcount += 1;
-        exist_stampcard.stamp_count = currente_stampcount;
+    end
 
-        if exist_stampcard.save
-            render json: exist_stampcard, status: 200 and return
-        else
-            render json: {error: 'cant stamp to stampcard'}, status: 404 and return
-        end
+    def show
 
-    end 
+    end
+
+    def index
+
+    end
 
     private
 
-    def stampcards_params
+    def stampcard_contents_params
         params.permit(:user_id, :stampcard_content_id, :pushed_stamp_count)
     end
 end
